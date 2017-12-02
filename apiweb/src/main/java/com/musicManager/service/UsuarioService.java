480784cd713f052eb323e4b44ec01c69beacd73a
Pedro Guedes <pedro.braga@ccc.ufcg.edu.br>
2017-12-02 00:34:58 -0200

    commit

--- apiweb/src/main/java/com/musicManager/service/UsuarioService.java
+++ apiweb/src/main/java/com/musicManager/service/UsuarioService.java
@@ -1,6 +1,9 @@
 package com.musicManager.service;
 
+import org.springframework.stereotype.Service;
+
 import com.musicManager.model.Usuario;
+
 
 public interface UsuarioService {
 	public void cadastrar(Usuario usuario);
